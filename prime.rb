895fe738d340a72f01653b194388e8514eda582a
Sean Murphy <filmmakersean@fastmail.net>
2019-05-24 18:13:31 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,15 +2,7 @@ def prime?(n)
   d = 2
   if n == 2 || n == 1
     return true
-  else
-    while d < n do
-      if n % d == 0
-        return false
-      else
-        d += 1
-      end
-    end
-  end
-end
+    elsif n % 2 == 0
+    return false
 
       
