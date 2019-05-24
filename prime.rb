44afee80c7393c03669d668e4c644682734d4e79
Sean Murphy <filmmakersean@fastmail.net>
2019-05-24 17:48:43 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+  if n <= 3 && !n == 0
+    return true
+    elsif n % 2 == 0 or n % 3 == 0
+    return false
+  end
+  i = 5
+  while i * i <= n do
+    if n % i == 0 || n % (i + 2) == 0
+      return false
+    end
+    return false
+    i += 6
+    
+end
